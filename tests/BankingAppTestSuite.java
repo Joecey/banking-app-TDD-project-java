29193c77774fa134a42f5edf6c1df6ca879033ad
joecey <joe@legitfit.com>
2025-12-16 17:22:19 +0000

    setup testSuite

---
+++ tests/BankingAppTestSuite.java
@@ -0,0 +1,14 @@
+import org.junit.platform.suite.api.SelectClasses;
+import org.junit.platform.suite.api.Suite;
+
+// Here, we select all the test classes we want to run
+@Suite
+@SelectClasses({
+        TestExample.class,
+        TestExample2.class
+})
+
+// This blank class is just needed for the testSuite to start running
+public class BankingAppTestSuite {
+
+}
--- tests/TestExample.java
+++ tests/TestExample.java
@@ -4,14 +4,6 @@ import org.junit.jupiter.api.Test;
 class TestExample {
     // Tests must be inside a @Test method to work
     @Test
-    void testStringEquality() {
-        String expected = "Hello World";
-        String actual = "Hello World";
-        assertEquals(expected, actual);
-    }
-
-    // Tests must be inside a @Test method to work
-    @Test
     void testAnotherTest() {
         String expected = "Hello World2";
         String actual = "Hello World2";
